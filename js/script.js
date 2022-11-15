2009a2966118f8921c2f5b8a3c850665430155c7
chamilou <chamilou@gmail.com>
2022-11-15 15:35:06 +0100

    nothung important in JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-fetch("https://images-api.nasa.gov/search?q=mars%2011...")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+let arr = []
+fetch("https://images-api.nasa.gov/search?q=mars%2011")
+    .then((response) => response.json())
+    .then((data) => {
+        arr = data;
+        console.log(data)
+
+        let sect = document.getElementsByTagName("section")[0]
+        sect.innerHTML = "Section";
+        for (let i = 0; i < data.length; i++) {
+
+            sect.innerHTML = arr.collection.items[0].links[0];
+            console.log(data.length)
+        }
+    });
