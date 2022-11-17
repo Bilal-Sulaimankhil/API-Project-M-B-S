91b8af6ad5b998af2fc728ee1c1f1b08cd6173b9
chamilou <chamilou@gmail.com>
2022-11-17 16:51:48 +0100

    clean up css

--- js/script.js
+++ js/script.js
@@ -12,11 +12,7 @@ dropdownMenu.addEventListener("click", (e) => {
     document.querySelector("#we").style.display = "none";
     document.querySelector("#galaxy").style.display = "none";
     document.querySelector("#planet_content").style.display = "flex";
-    content.childNodes.forEach((child) => child.remove());
     document.querySelector("#we").style.display = "none";
-    document.querySelector("#galaxy").style.display = "none";
-    document.querySelector("#planet_content").style.display = "block";
-
     let selectedPlanet = e.target.innerText;
     fetch(`https://images-api.nasa.gov/search?q=${selectedPlanet}%2011...`)
         .then((response) => response.json())
@@ -25,27 +21,20 @@ dropdownMenu.addEventListener("click", (e) => {
                 const figure = document.createElement("figure");
                 const image = document.createElement("img");
                 image.setAttribute("src", data.collection.items[index].links[0].href);
-                image.style.width = "300px;"
                 const figCaption = document.createElement("figcaption");
                 figCaption.innerText = data.collection.items[index].data[0].title;
-                // figure.style.display = "flex"
                 const p = document.createElement("p");
-                // p.style.width = "100px"
-                // p.style.textOverflow = "ellipsis"
-                // p.style.overflow = "hidden";
-
                 p.innerText = data.collection.items[index].data[0].description;
-
                 figure.appendChild(image);
                 figure.appendChild(figCaption);
                 figure.appendChild(p);
                 content.appendChild(figure);
             }
         });
-    figure.appendChild(image);
-    figure.appendChild(figCaption);
-    figure.appendChild(p);
-    content.appendChild(figure);
+    // figure.appendChild(image);
+    // figure.appendChild(figCaption);
+    // figure.appendChild(p);
+    // content.appendChild(figure);
 
     // imgTag1 = document.querySelector("#planet_image1");
     // imgTag2 = document.querySelector("#planet_image2");
@@ -54,10 +43,3 @@ dropdownMenu.addEventListener("click", (e) => {
     // imgTag2.setAttribute("src", imageUrl2);
     // imgTag3.setAttribute("src", imageUrl3);
 });
-
-// daily photo
-// fetch(
-//   "https://api.nasa.gov/planetary/apod?api_key=BkF3xesYsarNQpqzChE7TeVb99xdob6GnEdWdfaE"
-// )
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
