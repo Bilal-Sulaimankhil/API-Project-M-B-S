91398d6baed50218b8da3f42b3bb91d798827271
MuratGuld <slimplus1952@gmail.com>
2022-11-17 16:48:05 +0100

    responsive design is added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-// nasa search
-// planets
-// fetch("https://images-api.nasa.gov/search?q=mars%2011...")
-//   .then((response) => response.json)()
-//   .then((json) => console.log(json));
-
 let content = document.querySelector("#planet_content");
 let dropdownMenu = document.querySelector(".dropdown");
 
@@ -33,10 +27,3 @@ dropdownMenu.addEventListener("click", (e) => {
       }
     });
 });
-
-// daily photo
-// fetch(
-//   "https://api.nasa.gov/planetary/apod?api_key=BkF3xesYsarNQpqzChE7TeVb99xdob6GnEdWdfaE"
-// )
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
